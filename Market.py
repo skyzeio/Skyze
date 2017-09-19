5bacd0fc7f1d320bafddd2476dcde6f77b6c02b7
Luke Hamilton <luke@mettadata.com>
2017-09-19 20:08:54 +1000

    Added line of code to create a directory path if it doesnt exist

--- Market.py
+++ Market.py
@@ -1,4 +1,4 @@
-       
+
 #----------------------------------------------------------------------------------------------------------
 #
 #   CLASS Market
@@ -27,74 +27,74 @@ import ExceptionSkyzeAbstract
 
 
 class Market():
-    
+
 
     def __init__(self, p_market_name, p_exchange, p_interval, p_market_data = [] ):
             self.market_name = p_market_name
             self.exchange = p_exchange
             self.interval = p_interval
             self.market_data = p_market_data
-            
-    
+
+
     @classmethod
     def fromTesting(cls, p_market_name , p_market_data = []):
             return cls(p_market_name,"","",p_market_data)
 
-    
+
     def getMarketName( self ):
-        return self.market_name 
+        return self.market_name
 
-    
+
     def getInterval( self ):
-        return self.interval 
+        return self.interval
 
-    
+
     def getExchange( self ):
-        return self.exchange 
-    
-    
-        
-    
+        return self.exchange
+
+
+
+
     # creates the full path and file name
     def constructFileName( self, p_testing ):
         file_name = ""
         if p_testing:
             file_name = os.path.join(settings.testing_file_path, "%s.csv" % p_market)
         else:
-            file_name = os.path.join( settings.data_file_path, 
+            file_name = os.path.join( settings.data_file_path,
                                       "%s.csv" % ( settings.exchanges.get_value(self.exchange,"Directory_name") + "/"
                                                   + self.interval + "/"
-                                                  + self.market_name) 
+                                                  + self.market_name)
                                     )
-            
-            
-        return file_name
-                
 
-        
+
+        return file_name
+
+
+
     #    Read Data from CSV
     #    INPUT
     #           p_market    String
     #    OUTPUT
     #            pandas DataFrame
     #
-    
-    def readMarketDataCSV( self, p_market = "NOTPASSED", p_testing = False ): 
-        "Opens the file and reads the data" 
-        
+
+    def readMarketDataCSV( self, p_market = "NOTPASSED", p_testing = False ):
+        "Opens the file and reads the data"
+
         # default parameter handling
-        if p_market == "NOTPASSED" : 
+        if p_market == "NOTPASSED" :
             p_market = self.market_name
 #         p_market = "Bitcoin"
 
         file_path = self.constructFileName(p_testing)
         print("File Path: " + file_path)
-        
-        
+
+
         try:
             # df is type <class 'pandas.core.frame.DataFrame'>
             df = pd.read_csv(
-                        file_path, 
+                        file_path,
                         header=None ,
                         names = [
                                   "Date", "Open", "High", "Low",
@@ -115,15 +115,15 @@ class Market():
         else:
             # Convert the date column to a date !
 #             d['Date'] = pd.to_datetime(d['Date'].astype(str), format='%Y%m%d')  # NOT NEEDED after making date the index column
-              
+
             df.index = [parser.parse(str(d)) for d in df["Date"]]
             del df["Date"]
-            return df 
-        
-        
-        
+            return df
+
+
+
     def saveMarketDataV01 (self, p_file_path = "", p_save_type="CSV" ):
-        # Save the file to CSV 
+        # Save the file to CSV
         my_file = Path(p_file_path)
         if my_file.is_file():
             # File exists
@@ -139,11 +139,11 @@ class Market():
                 # Reverse it so it is date ascending (newest at the end)
                 wr.writerows(reversed(self.market_data))
         return
-        
-        
-        
+
+
+
     def saveMarketData (self, p_file_path = "", p_save_type="CSV" ):
-        # Save the file to CSV 
+        # Save the file to CSV
         my_file = Path(p_file_path)
         if my_file.is_file():
             # File exists
@@ -155,9 +155,12 @@ class Market():
 #                 print(len(self.market_data))
 #                 wr.writerows(list(reversed(self.market_data)))
         else:
+            # Create directory paths
+            os.makedirs(os.path.dirname(p_file_path), exist_ok=True)
             # No file so create new file
-            self.market_data.to_csv(p_file_path, header=False, date_format='%Y-%m-%d %H:%M:%S')
-            
+            with open(p_file_path, 'w') as myfile:
+                self.market_data.to_csv(p_file_path, header=False, date_format='%Y-%m-%d %H:%M:%S')
+
 #             with open(p_file_path, 'w') as myfile:
 #                 wr = csv.writer(myfile)
 #                 # Reverse it so it is date ascending (newest at the end)
