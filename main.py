5c325e1abef3c1ae6d1d40610b4bbefe2cf24763
mikenew <mikenew16@gmail.com>
2017-09-20 13:46:57 +1000

    Cleaned up code Added comments

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-# 
-# Data
-#
-# Reads in Market Data Files and Prints Statistics
-#
-# Bugs 
+'''
+Created on 21/08/2017
+@author: michaelnew
 
-# from Exception import BaseException
+Used like a comandline with switches to run various functions
 
-#--- Imports -----------------------------------------------------------------------------------
+'''
+
+#--- Imports ---------------------------------------------------------------------------------------------
 import datetime
 from Market_Data.CoinMarketCap import CoinMarketCap
 from Market_Data.Cryptopia import Cryptopia
 from Market_Data.PoloniexSkyze import PoloniexSkyze
 from Portfolio import Portfolio
-#--- Global Variables  -----------------------------------------------------------------------------------
      
                 
 #----------------------------------------------------------------------------------------------------------
@@ -23,28 +21,31 @@ from Portfolio import Portfolio
 #----------------------------------------------------------------------------------------------------------
 
 
+
+# === Run Switches =====
+# This runs various functions - I use this to tesst things at the moment
 run_strategy    = False
 run_statistics  = False
 run_cryptopia   = False
 run_poloniex    = False
 
+# === Data load Switches =====
 cmc_load        = False
 cryptopia_load  = True
-poloniex_load   = True
-load_type       = "all"     # all | ico ! custom_list | do_nothing
+poloniex_load   = False
+
+# === Data load Type =====
+load_type       = "custom_list"     # Laod_type Options are:
+                                    #        all             ... the exchanege will call get all markets then download all
+                                    #        ico             ... will use the custom ICO list
+                                    #        custom_list     ... will use the custom list
+                                    #        do_nothing      ... won't do anything ... can be do nothing or any text other than above options
 
 
-# custom_list = [ 'fuda-energy', 'golos-gold', 'growers-international', 'fibocoins', 'falcoin', 'deuscoin', 
-#                 'bagcoin', 'gycoin', 'fargocoin', 'lepaoquan', 'pura', 'dynamiccoin', 'sigmacoin', 'techshares', 
-#                 'bitfid', 'asset-backed-coin', 'infchain', 'achain', 'health-care-chain', 'vechain', 'walton', 
-#                 'xenixcoin', 'caliphcoin', 'digital-money-bits', 'swaptoken', 'future-digital-currency', 'powercoin', 
-#                 'eboostcoin', 'krypstal', 'gxshares', 'byteball', 'ark', 'factom', 'dogecoin', 'siacoin', 'steem', 
-#                 'stellar', 'bytecoin-bcn', 'bitshares', 'waves', 'zcash', 'stratis', 'lisk', 'bitconnect', 'hshare', 
-#                 'neo', 'ethereum-classic', 'monero', 'iota', 'nem', 'dash', 'litecoin', 'ripple', 'bitcoin-cash', 
-# custom_list_cmc = [ 'minex']        # asset
-# custom_list_cmc = [ 'stellar' ]
+
+# === Custom Load Lists =====
 custom_list_cmc = [ 'bitcoin']      # currency
-custom_list_cryptopia = [ 'ETH_BTC'] # 10/9/17 20:32 ROW 982
+custom_list_cryptopia = ['MST_BTC', 'BCN_DOGE', 'BCN_LTC', 'XRA_BTC', 'SKR_DOGE', 'LIT_BTC'] 
 custom_list_poloniex = ['BTC_AMP'] #, 'BTC_ARDR', 'BTC_BCH', 'BTC_BCN', 'BTC_BCY', 'BTC_BELA', 'BTC_BLK', 'BTC_BTCD']
 
 
@@ -53,29 +54,34 @@ custom_list_poloniex = ['BTC_AMP'] #, 'BTC_ARDR', 'BTC_BCH', 'BTC_BCN', 'BTC_BCY
 #    MAIN BODY
 #
 #----------------------------------------------------------------------------------------------------------
+# Track runtime
 start_time = datetime.datetime.now()
 print('=== Begin ========== '+ str(start_time)+ ' ========== '); print()
 
 
 portfolio = Portfolio()
 
+
+# For Strategy Testing
 if run_strategy:
 #     strat = StrategySTC()
     pass
 
-# print(cmc.scrapeAllCurrencies())
 
+
+# For statistics testing
 if run_statistics:
-    bruce = portfolio.getMarketsICO()
-    mkt_stat = portfolio.calculateStats(bruce, "Coin Market Cap", "DAY_1")   #WORKING ON RETURNING DATAFRAME
+    ico_markets = portfolio.getMarketsICO()
+    mkt_stat = portfolio.calculateStats(ico_markets, "Coin Market Cap", "DAY_1")   #WORKING ON RETURNING DATAFRAME
     print("mkt_stat: " + str(mkt_stat.head(5)))
     print("describe: " + str(mkt_stat.describe()))
     print("describe 2: " + str(mkt_stat.describe(percentiles=[.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,.95])[[0,1,4,5]]))
     print(type(mkt_stat.describe(percentiles=[.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,.95])[[0,1,4,5]]))
     portfolio.saveStatsToExcel( mkt_stat )
-    # mkt_deta?arketDetailsToCSV(mkt_details)     # All markets
    
-
+   
+   
+# Data load switches
 if load_type == "all":
     print("=== UPDATE MARKET DATA - ALL ===")
     if cmc_load: 
@@ -106,7 +112,10 @@ elif load_type == "custom_list":
     if poloniex_load: 
         poloniex = PoloniexSkyze()
         poloniex.updateMarketData(custom_list_poloniex)
-    
+
+
+
+# Run Switches
 if run_cryptopia == True:
     cryptopia = Cryptopia()
     print(cryptopia.getAllMarkets())
@@ -115,8 +124,9 @@ if run_poloniex == True:
     poloniex = PoloniexSkyze()
 #     print(poloniex.getAllMarkets())
     poloniex.updateMarketData(custom_list_poloniex)
-    
-#pprint(cmc.markets)
+
+
+# Calculate and print run time
 end_time = datetime.datetime.now() 
 run_time = end_time-start_time
 print(); print( '=== End ========== '+ str(end_time) + ' ========== ' + " run time: "+ str(run_time) + ' ========== ' ) 
