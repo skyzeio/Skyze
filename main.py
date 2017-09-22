3c1c15f86d51c322af0907457d917b8b1b241b52
mikenew <mikenew16@gmail.com>
2017-09-22 12:16:42 +1000

    Splitting the main files so we can modularise the Market_Data system
    Moved the data loading code to the file main_update_data in the
    Market_Data package

--- main.py
+++ main.py
@@ -2,18 +2,17 @@
 Created on 21/08/2017
 @author: michaelnew
 
+MAIN - used to run general functions .. until split off into their own main....py files
+
 Used like a comandline with switches to run various functions
 
 '''
 
 #--- Imports ---------------------------------------------------------------------------------------------
 import datetime
-from Market_Data.CoinMarketCap import CoinMarketCap
-from Market_Data.Cryptopia import Cryptopia
-from Market_Data.PoloniexSkyze import PoloniexSkyze
 from Portfolio import Portfolio
-     
-                
+
+
 #----------------------------------------------------------------------------------------------------------
 #
 #    RUN SWITCHES
@@ -28,27 +27,6 @@ run_strategy    = False
 run_statistics  = False
 run_cryptopia   = False
 run_poloniex    = False
-
-# === Data load Switches =====
-cmc_load        = False
-cryptopia_load  = False
-poloniex_load   = True
-
-# === Data load Type =====
-load_type       = "all"     # Laod_type Options are:
-                                    #        all             ... the exchanege will call get all markets then download all
-                                    #        ico             ... will use the custom ICO list
-                                    #        custom_list     ... will use the custom list
-                                    #        do_nothing      ... won't do anything ... can be do nothing or any text other than above options
-
-
-
-# === Custom Load Lists =====
-custom_list_cmc = [ 'bitcoin']      # currency
-custom_list_cryptopia = ['MST_BTC', 'BCN_DOGE', 'BCN_LTC', 'XRA_BTC', 'SKR_DOGE', 'LIT_BTC'] 
-custom_list_poloniex = ['ETH_CVC'] #, 'BTC_ARDR', 'BTC_BCH', 'BTC_BCN', 'BTC_BCY', 'BTC_BELA', 'BTC_BLK', 'BTC_BTCD']
-
-
 #----------------------------------------------------------------------------------------------------------
 #
 #    MAIN BODY
@@ -56,7 +34,7 @@ custom_list_poloniex = ['ETH_CVC'] #, 'BTC_ARDR', 'BTC_BCH', 'BTC_BCN', 'BTC_BCY
 #----------------------------------------------------------------------------------------------------------
 # Track runtime
 start_time = datetime.datetime.now()
-print('=== Begin ========== '+ str(start_time)+ ' ========== '); print()
+print('=== Begin Main General ========== '+ str(start_time)+ ' ========== '); print()
 
 
 portfolio = Portfolio()
@@ -78,41 +56,6 @@ if run_statistics:
     print("describe 2: " + str(mkt_stat.describe(percentiles=[.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,.95])[[0,1,4,5]]))
     print(type(mkt_stat.describe(percentiles=[.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,.95])[[0,1,4,5]]))
     portfolio.saveStatsToExcel( mkt_stat )
-   
-   
-   
-# Data load switches
-data_load_switch = cmc_load or cryptopia_load or poloniex_load
-if load_type == "all" and data_load_switch:
-    print("=== UPDATE MARKET DATA - ALL ===")
-    if cmc_load: 
-        cmc = CoinMarketCap()
-        cmc.updateMarketData()
-    if cryptopia_load: 
-        crypt = Cryptopia()
-        crypt.updateMarketData()
-    if poloniex_load: 
-        poloniex = PoloniexSkyze()
-        poloniex.updateMarketData()
-elif load_type == "ico" and data_load_switch:
-    print("=== UPDATE MARKET DATA - ICO ===")
-    if cmc_load: 
-        cmc = CoinMarketCap()
-        mkt_data = cmc.updateMarketData(portfolio.markets_ICO)
-    if cryptopia_load: 
-        crypt = Cryptopia()
-        crypt.updateMarketData(portfolio.cryptopia_ICO)
-elif load_type == "custom_list" and data_load_switch:
-    if cmc_load: 
-        print("=== UPDATE MARKET DATA - CUSTOM ===")
-        cmc = CoinMarketCap()
-        cmc.updateMarketData(custom_list_cmc)
-    if cryptopia_load: 
-        crypt = Cryptopia()
-        crypt.updateMarketData(custom_list_cryptopia)
-    if poloniex_load: 
-        poloniex = PoloniexSkyze()
-        poloniex.updateMarketData(custom_list_poloniex)
 
 
 
@@ -131,6 +74,6 @@ if run_poloniex == True:
 
 
 # Calculate and print run time
-end_time = datetime.datetime.now() 
+end_time = datetime.datetime.now()
 run_time = end_time-start_time
-print(); print( '=== End ========== '+ str(end_time) + ' ========== ' + " run time: "+ str(run_time) + ' ========== ' ) 
+print(); print( '=== End ========== '+ str(end_time) + ' ========== ' + " run time: "+ str(run_time) + ' ========== ' )
