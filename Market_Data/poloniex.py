34029c132537d31bbf6316aa74207ccec28492ad
mikenew <mikenew16@gmail.com>
2017-09-22 16:00:20 +1000

    removed keys from file and put them in the environment variable

--- Market_Data/poloniex.py
+++ Market_Data/poloniex.py
@@ -11,6 +11,7 @@ import urllib2
 import json
 import time
 import hmac,hashlib
+import os               # for accessing environment variables
 
 def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
     return time.mktime(time.strptime(datestr, format))
@@ -18,9 +19,10 @@ def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
 class poloniex:
     def __init__(self, APIKey, Secret):
         # TODO move the keys to a local settings file or encrypt them ??
-        self.APIKey = "AQMOOBX5-N52FGSW2-8QJQ90WF-JEFH4324"
-        self.Secret = "c530bb4184f91c779dcd3d4d46e70179579e4a556df6504e85f5189859e898ddb0b601d1c4aaf821808da8c423dc473e3ff62b1f955d21728459a94a55059c28"
+        self.APIKey = os.environ['POLONIEX_PROD_APIKEY']
+        self.Secret = os.environ['POLONIEX_PROD_SECRET']
 
+        
     def post_process(self, before):
         after = before
 
@@ -31,7 +33,7 @@ class poloniex:
                     if(isinstance(after['return'][x], dict)):
                         if('datetime' in after['return'][x] and 'timestamp' not in after['return'][x]):
                             after['return'][x]['timestamp'] = float(createTimeStamp(after['return'][x]['datetime']))
-                            
+
         return after
 
     def api_query(self, command, req={}):
@@ -75,7 +77,7 @@ class poloniex:
 
 
     # Returns all of your balances.
-    # Outputs: 
+    # Outputs:
     # {"BTC":"0.59098578","LTC":"3.31117268", ... }
     def returnBalances(self):
         return self.api_query('returnBalances')
@@ -83,7 +85,7 @@ class poloniex:
     # Returns your open orders for a given market, specified by the "currencyPair" POST parameter, e.g. "BTC_XCP"
     # Inputs:
     # currencyPair  The currency pair e.g. "BTC_XCP"
-    # Outputs: 
+    # Outputs:
     # orderNumber   The order number
     # type          sell or buy
     # rate          Price the order is selling or buying at
@@ -96,7 +98,7 @@ class poloniex:
     # Returns your trade history for a given market, specified by the "currencyPair" POST parameter
     # Inputs:
     # currencyPair  The currency pair e.g. "BTC_XCP"
-    # Outputs: 
+    # Outputs:
     # date          Date in the form: "2014-02-19 03:44:59"
     # rate          Price the order is selling or buying at
     # amount        Quantity of order
@@ -110,7 +112,7 @@ class poloniex:
     # currencyPair  The curreny pair
     # rate          price the order is buying at
     # amount        Amount of coins to buy
-    # Outputs: 
+    # Outputs:
     # orderNumber   The order number
     def buy(self,currencyPair,rate,amount):
         return self.api_query('buy',{"currencyPair":currencyPair,"rate":rate,"amount":amount})
@@ -120,7 +122,7 @@ class poloniex:
     # currencyPair  The curreny pair
     # rate          price the order is selling at
     # amount        Amount of coins to sell
-    # Outputs: 
+    # Outputs:
     # orderNumber   The order number
     def sell(self,currencyPair,rate,amount):
         return self.api_query('sell',{"currencyPair":currencyPair,"rate":rate,"amount":amount})
@@ -129,17 +131,17 @@ class poloniex:
     # Inputs:
     # currencyPair  The curreny pair
     # orderNumber   The order number to cancel
-    # Outputs: 
+    # Outputs:
     # succes        1 or 0
     def cancel(self,currencyPair,orderNumber):
         return self.api_query('cancelOrder',{"currencyPair":currencyPair,"orderNumber":orderNumber})
 
-    # Immediately places a withdrawal for a given currency, with no email confirmation. In order to use this method, the withdrawal privilege must be enabled for your API key. Required POST parameters are "currency", "amount", and "address". Sample output: {"response":"Withdrew 2398 NXT."} 
+    # Immediately places a withdrawal for a given currency, with no email confirmation. In order to use this method, the withdrawal privilege must be enabled for your API key. Required POST parameters are "currency", "amount", and "address". Sample output: {"response":"Withdrew 2398 NXT."}
     # Inputs:
     # currency      The currency to withdraw
     # amount        The amount of this coin to withdraw
     # address       The withdrawal address
-    # Outputs: 
+    # Outputs:
     # response      Text containing message about the withdrawal
     def withdraw(self, currency, amount, address):
         return self.api_query('withdraw',{"currency":currency, "amount":amount, "address":address})
