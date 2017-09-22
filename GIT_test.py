5b567c3a7386b78539c41f1a9cf7fc9ec3dd90a5
mikenew <mikenew16@gmail.com>
2017-09-22 16:50:07 +1000

    slack test

--- GIT_test.py
+++ GIT_test.py
@@ -3,7 +3,9 @@ Created on 14/09/2017
 asdfasdf
 @author: michaelnew
 
-after branch 
+after branch
+
+testing the GIT / SLACK integration
 
 
 '''
@@ -18,4 +20,3 @@ class MyClass(object):
         '''
         Constructor
         '''
-        
