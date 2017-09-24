60fae8c1ff343193e513d6aeda5e9d33af6911b2
mikenew <mikenew16@gmail.com>
2017-09-24 12:38:12 +1000

    removed date column and added it as a datetime index

--- Unit_Test/SkyzeUnitTest.py
+++ Unit_Test/SkyzeUnitTest.py
@@ -7,6 +7,7 @@ Created on 05/09/2017
 import os
 import unittest
 import pandas as pd
+from dateutil import parser
 
 # Skyze Libraries
 import settings
@@ -18,23 +19,24 @@ class SkyzeUnitTest(unittest.TestCase):
     '''
     classdocs
     '''
-        
-        
-    def readTargetResults( self, p_results_file, p_column_names ): 
-        "Opens the file and reads the data" 
-        
+
+
+    def readTargetResults( self, p_results_file, p_column_names ):
+        "Opens the file and reads the data"
+
     #     print("File Path: " + settings.data_file_path)
         file_path = os.path.join(settings.results_file_path, "%s.csv" % p_results_file)
-        
-        
+
+        column_names = ["Date"] + p_column_names
+
         try:
             # d is type <class 'pandas.core.frame.DataFrame'>
-            d = pd.read_csv(
-                        file_path, 
-                        header=None ,
-                        names = p_column_names,
-                        skiprows = 1
-                        )
+            target_results = pd.read_csv(
+                                            file_path,
+                                            header=None ,
+                                            names = column_names,
+                                            skiprows = 1
+                                        )
         except IOError as err:
             print("File Error:   " + file_path)
             raise IOError ("FileNotFound","EXCEPTION SkyzeUnitTest::readTargetResults .... IOError File does not exist")
@@ -45,9 +47,11 @@ class SkyzeUnitTest(unittest.TestCase):
             print(sys.exc_info())
             return
         else:
-            # Convert the date column to a date !    
-            print(d)        
-            d['Date'] = pd.to_datetime(d['Date'].astype(str), format='%Y%m%d')
-              
-            return d  
-        
+            # Convert the date column to a date ! ...... Not needed now date is the index
+            #target_results['Date'] = pd.to_datetime(target_results['Date'].astype(str), format='%Y%m%d')
+
+            # Move the date column to the index
+            target_results.index = [parser.parse(str(d)) for d in target_results["Date"]]
+            del target_results["Date"]
+
+            return target_results
