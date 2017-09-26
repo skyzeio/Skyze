dbfad8bec6ba0044091a6e0e3994fb12febc27d8
mikenew <mikenew16@gmail.com>
2017-09-26 14:55:09 +1000

    refining unit testing code

--- Unit_Test/Indicators/SuperTrend_test.py
+++ Unit_Test/Indicators/SuperTrend_test.py
@@ -4,7 +4,9 @@ Created on 05/09/2017
 @author: michaelnew
 '''
 
+
 # Standard Libraries
+
 
 # Our libraries - Required
 from Unit_Test.UnitTestSkyzeAbstract import *       # Parent import
@@ -23,16 +25,16 @@ class SuperTrend_test(UnitTestSkyzeAbstract):
 
 
 
-
     def test(self):
 
         # Test Parameters
         output_info    = True
-        target_file    = "Results-bitcoin-SuperTrend"
+        target_file    = "Target-Results-SuperTrend-bitcoin"
         test_file      = "bitcoin_TEST"
-        target_columns = ["H-L","H-PC","L-PC","True_Range","ATR_15","basic_ub","basic_lb","final_ub","final_lb","Super_Trend"]
-        test_columns   = ["H-L","H-PC","L-PC","True_Range","ATR_15","basic_ub","basic_lb","final_ub","final_lb","Super_Trend"]
+        target_columns = ["Open","High","Low","Close","Volume","MarketCap","H-L","H-PC","L-PC","True_Range","ATR_15","basic_ub","basic_lb","final_ub","final_lb","Super_Trend"]
+        test_columns   = ["Open","High","Low","Close","Volume","MarketCap","H-L","H-PC","L-PC","True_Range","ATR_15","basic_ub","basic_lb","final_ub","final_lb","Super_Trend"]
 
+        # Indicator Parameters
         st_period = 15
         st_multiplier = 3
         st_name_extension = ""
@@ -40,12 +42,14 @@ class SuperTrend_test(UnitTestSkyzeAbstract):
         # Output Headings
         print ("\n\n======= This is a test of SUPERTREND INDICATOR ==============")
         print ("Test data: " + test_file + "    \nTarget data: " + target_file)
-        print ("Columns: "+ str(test_columns))
-        print ("Parameters: period: " + str(st_period) + "   Multiplier: " + str(st_multiplier))
+        print ("Columns: " + str(len(test_columns)) + " ... "+ str(test_columns))
+        print ("Parameters: ")
+        print ("     period: " + str(st_period) + "   Multiplier: " + str(st_multiplier))
 
         # Get the data
         mkt = Market.fromTesting(test_file)
         mkt_data = mkt.readMarketDataCSV(p_testing=True)
+        print("Rows in mkt data: "+str(len(mkt_data)))
 
         # Calcualte the Indicators
         supertrend = SuperTrend(st_period,st_multiplier,st_name_extension)
@@ -53,35 +57,20 @@ class SuperTrend_test(UnitTestSkyzeAbstract):
 
         # Read in the target results
         target_data = self.readTargetResults(target_file, target_columns)
+        print("\n\n")
 
         # Format boolean columns
         # None                       # convert to boolean
 
-        # Output the Testing Results
-        if output_info:
-            print();print();print(); print("=== Target_data . head  === === === === === ")
-            print(target_data.head(16))
-            print(); print("=== Target_data . tail === === === === === ")
-            print(target_data.tail(5))
+        # Output the Testing Info
+        self.printTestInfo( output_info, mkt_data, target_data, "SuperTrend" )
 
-            print();print();print(); print("=== Market data Results . head === === === === === ")
-            print(mkt_data.head(16)) #[test_columns]
-            print(); print("=== Market data Results . tail === === === === === ")
-            print(mkt_data.tail(5)) # [test_columns]
+        # Assert by series
+        self.assertBySeries( output_info, mkt_data, target_data, target_columns )
 
-        # Assertions
-        if output_info:
-            print(); print("=== Series Equal DATE === === === === === ")
-#             assert_series_equal( mkt_data["Date"], target_data["Date"])
-            print("PASS")
-#             for test_column in target_columns:
-#                 print(); print("=== Series Equal %s === === === === === " % test_column)
-#                 assert_series_equal( mkt_data[test_column], target_data[test_column])
-#                 print("PASS")
-#
-#         print(); print("=== DataFrame Equal === === === === === ")
-#         assert_frame_equal( mkt_data[test_columns], target_data)
-#         print("PASS")
+        # Assert the data frame
+        print(); print("=== DataFrame Equal === === === === === ")
+        self.dataframe_assert( "Final Results", mkt_data, target_data)
 
 
 if __name__ == '__main__':
--- Unit_Test/UnitTestSkyzeAbstract.py
+++ Unit_Test/UnitTestSkyzeAbstract.py
@@ -11,6 +11,7 @@ from pandas.util.testing import assert_frame_equal
 from pandas.util.testing import assert_series_equal
 import pandas as pd
 from dateutil import parser
+from pandas   import ExcelWriter
 
 # Skyze Libraries
 import settings
@@ -21,7 +22,116 @@ import settings
 class UnitTestSkyzeAbstract(unittest.TestCase):
     '''
     classdocs
+
+    1. Creates a list of assertion errors rather than stopping after each error.
+       See https://stackoverflow.com/questions/4732827/continuing-in-pythons-unittest-when-an-assertion-fails
+
     '''
+    def setUp(self):
+        self.assertion_errors = []
+
+
+
+    def tearDown(self):
+        print ("\n\n\n")
+        try:
+            self.assertEqual([], self.assertion_errors)
+        except AssertionError as err:
+            # Assertion Failure
+            print("=== Test Failed === === === === === ")
+            print("List of Assertion Errors:")
+            for assertion_error in self.assertion_errors:
+                print("\n\n=== Name: "+assertion_error[0])
+                print(assertion_error[1])
+        else:
+            # Assertion Passed
+            print("=== Test Passed === === === === === ")
+            print("PASS: ALL TESTS .. You Rock :)")
+
+        print("\n\nLeft = test results ..... Right = target results")
+        return
+
+
+
+
+
+    def series_assert( self, p_name, p_test_results, p_target_results ):
+        ''' Asserts two series on the same column name
+            Stores errors (exceptions) in a list so that all tests are run '''
+
+        try:
+            assert_series_equal(    p_test_results[p_name],
+                                    p_target_results[p_name],
+                                    check_exact = False,
+                                    check_less_precise = False
+                                )
+
+        except AssertionError as e:
+            # Assertion Failure
+            self.assertion_errors.append([p_name,str(e)])
+            print("\n--- FAIL: "+p_name)
+
+            # Get the rows that are different
+            diffs = pd.DataFrame({'test':p_test_results[p_name], 'target':p_target_results[p_name]})
+            diffs["Different"] = diffs["test"] != diffs["target"]
+            print(diffs.loc[diffs.Different == True])
+        else:
+            # Assertion Passed
+            print("\n+++ PASS: "+p_name)
+
+        return
+
+
+
+
+
+    def dataframe_assert( self, p_name, p_test_results, p_target_results ):
+        ''' Asserts two dataframes
+            Stores errors (exceptions) in a list so that all tests are run '''
+
+        try:
+            assert_frame_equal( p_test_results, p_target_results)
+        except AssertionError as err:
+            # Assertion Failure
+            self.assertion_errors.append([p_name,str(err)])
+            print("DATAFRAME FAIL: "+p_name)
+        else:
+            # Assertion Passed
+            print("DATAFRAME PASS: "+p_name)
+
+        return
+
+
+
+
+
+    def printTestInfo( self, p_output, p_mkt_data, p_target_data, p_file_name):
+        if p_output:
+            print();print();print(); print("=== Target_data . head  === === === === === ")
+            print(p_target_data.head(16))
+            print(); print("=== Target_data . tail === === === === === ")
+            print(p_target_data.tail(5))
+
+            print();print();print(); print("=== Test Results . head === === === === === ")
+            print(p_mkt_data.head(16)) #[test_columns]
+            print(); print("=== Test Results . tail === === === === === ")
+            print(p_mkt_data.tail(5)) # [test_columns]
+
+            # Save market data to excel
+            #self.saveTestResults( p_mkt_data, "Test-Output-"+p_file_name, p_testing = True)
+        return
+
+
+
+    def assertBySeries( self, p_output, p_mkt_data, p_target_data, p_target_columns):
+        if p_output:
+            print(); print()
+            print("=== Series Assertion === === === === === ")
+
+            # loop through the series
+            for test_column in p_target_columns:
+                self.series_assert( test_column, p_mkt_data, p_target_data)
+
 
 
     def readTargetResults( self, p_results_file, p_column_names ):
@@ -53,9 +163,36 @@ class UnitTestSkyzeAbstract(unittest.TestCase):
         else:
             # Convert the date column to a date ! ...... Not needed now date is the index
             #target_results['Date'] = pd.to_datetime(target_results['Date'].astype(str), format='%Y%m%d')
-            
+
             # Move the date column to the index
             target_results.index = [parser.parse(str(d)) for d in target_results["Date"]]
             del target_results["Date"]
 
             return target_results
+
+
+
+
+
+
+    def saveTestResults (self, p_results, p_file_name, p_file_path = "", p_testing = False ):
+        ''' Export to excel file '''
+
+        # set file path
+        file_path = p_file_path
+        if p_file_path == "":
+            if p_testing:
+                file_path = settings.testing_file_path
+            else:
+                file_path = settings.data_file_path
+
+    #         xlApp=win32com.client.Dispatch("Excel.Application")
+    #         wb = xlApp.Workbooks.Open(Filename="C:\Full Location\To\excelsheet.xlsm")
+
+        # Write to Excel
+        print("Test Output: "+file_path+"/"+p_file_name+".xlsx")
+        writer = ExcelWriter(file_path+"/"+p_file_name+".xlsx")
+        p_results.to_excel(writer,'Results')
+        writer.save()
+
+        return
