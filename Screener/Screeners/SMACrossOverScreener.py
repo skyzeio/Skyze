726b9ff9e69a21e4eaac0582eef2589c023ff457
mikenew <mikenew16@gmail.com>
2017-10-11 15:08:39 +1100

    Re organised Screener for micro-serivce

---
+++ Screener/Screeners/SMACrossOverScreener.py
@@ -0,0 +1,49 @@
+"""Created on 11/10/2017
+@author: michaelnew"""
+# Library Imports
+# 3rd parties
+
+# Our Library
+from Screeners.ScreenerAbstract import *
+from Indicators.SimpleMovingAverage import SimpleMovingAverage
+from Indicators.Crosses import Crosses
+import ExceptionSkyzeAbstract
+
+
+class SMACrossOverScreener(ScreenerAbstract):
+    """Class doc"""
+
+    # Static Class Variables
+    name = "SuperTrend CrossOver Screener"
+    version = 1.0
+    description = "Uses a fast and slow STC crossover combined with a long EMA and big fat candle to trigger trade signals."
+
+    def __init__(self):
+
+    @classmethod
+    def getName(self):
+        """ Getter """
+        return self.name
+
+    @classmethod
+    def getVersion(self):
+        """ Getter """
+        return self.version
+
+    @classmethod
+    def getDescription(self):
+        """ Getter """
+        return self.description
+
+    @classmethod
+    def getResult(self):
+        """ Getter """
+        return sel
+
+
+f.result
+
+    def calculateSignals(self,
+                         p_data        # pd dataframe series
+                         ):
+        """Calculates and returns screener signals"""
