67b36a2d3d7e32b9d7a6999e45400bad78a4df52
mikenew <mikenew16@gmail.com>
2017-10-12 16:27:57 +1100

    Standard Library convention

---
+++ Skyze_Standard_Library/SkyzeSerivceAbstract.py
@@ -0,0 +1,22 @@
+"""Created on 12/10/2017
+   @author: michaelnew"""
+
+# Third Party Imports
+from datetime import datetime
+import json
+
+# Skyze Imports
+import ExceptionSkyzeAbstract
+
+
+class SkyzeServiceAbstract(ExceptionSkyzeAbstract):
+    """Skyze Base class for services"""
+
+    def __init__(self, message_bus):
+        """Constructor"""
+        self.__created = datetime.now()
+        self.__message_bus = message_bus
+        print(f"Log: {self.getType()} Started")
+
+    def getType(self):
+        return self.__class__.__name__
