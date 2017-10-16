39856abf0affeaa84e7bc5739dfe864ae187ff57
mikenew <mikenew16@gmail.com>
2017-10-17 09:51:55 +1100

    Added new message types

--- Skyze_Messaging_Service/Messages/SkyzeMessageTypes.py
+++ Skyze_Messaging_Service/Messages/SkyzeMessageTypes.py
@@ -8,4 +8,4 @@ from enum import Enum
 SkyzeMessageType = Enum("MessageType",
                         "NEW_MARKET_DATA SCREENER_RUN MARKET_DATA_UPDATER_RUN \
                         MARKET_DATA_UPDATER_RUN_ALL NOTIFICATION SCHEDULER_RUN \
-                        SIGNAL ORDER FILL SENTIMENT NULL")
+                        SCHEDULER_TEST SIGNAL ORDER FILL SENTIMENT NULL")
