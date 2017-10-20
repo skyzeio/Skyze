18a70035c02ac74cb72a80dc23a051ed5246686e
mikenew <mikenew16@gmail.com>
2017-10-20 15:02:05 +1100

    new message type and formatting

--- Skyze_Messaging_Service/Messages/SkyzeMessageTypes.py
+++ Skyze_Messaging_Service/Messages/SkyzeMessageTypes.py
@@ -4,8 +4,15 @@
 # Third Party Imports
 from enum import Enum
 
-
+# All messages types that can be sent by skyze services
 SkyzeMessageType = Enum("MessageType",
-                        "NEW_MARKET_DATA SCREENER_RUN MARKET_DATA_UPDATER_RUN \
-                        MARKET_DATA_UPDATER_RUN_ALL NOTIFICATION SCHEDULER_RUN \
-                        SCHEDULER_TEST SIGNAL ORDER FILL SENTIMENT NULL")
+                        "NEW_MARKET_DATA \
+                        SCREENER_RUN \
+                        MARKET_DATA_UPDATER_RUN \
+                        MARKET_DATA_UPDATER_RUN_ALL \
+                        MARKET_DATA_UPDATER_RUN_COMPLETE \
+                        NOTIFICATION \
+                        SCHEDULER_RUN \
+                        SCHEDULER_TEST SIGNAL")
+
+# Future order types: ORDER FILL SENTIMENT NULL
