2fdf827aa83bda1078fdbf63ebb45db9408f421e
mikenew <mikenew16@gmail.com>
2017-10-20 15:04:08 +1100

    added rollbar and logging

--- Skyze_Standard_Library/ExceptionSkyzeAbstract.py
+++ Skyze_Standard_Library/ExceptionSkyzeAbstract.py
@@ -10,33 +10,46 @@ import inspect
 import sys
 from pprint import pprint
 import json
+import rollbar
+
+# Skyze Imports
+from Skyze_Standard_Library.SkyzeLogger import *
 
 
 class ExceptionSkyzeAbstract(Exception):
-    '''
-    classdocs
-    '''
-# 
-# 
+    """classdocs"""
+#
+#
 #     def __init__(self):
 #         '''
 #         Constructor
 #         '''
 #         pass
-        
-    def printException(self, p_line_no, p_message):  
+
+    def reportException(self, class_name, p_line_no, p_message, logger, to_print=True, to_raise=True):
         trace_len = len(trace())
-        err_msg = "\nSKYZE EXCEPTION: " + self.__class__.__name__
-        err_msg += "::" + trace()[0].function
-        err_msg += "::" + str(trace()[0].lineno)
-        err_msg += "\n Called: " + trace()[trace_len-1].function
-        err_msg += "   on line: " + str(trace()[trace_len-1].lineno)
-        err_msg += "\n code Context: " + str(trace()[trace_len-1].code_context)
+        err_msg = "\nSKYZE EXCEPTION: " + class_name
+        err_msg += "::" + trace()[1].function
+        err_msg += "::" + str(trace()[1].lineno)
+        err_msg += "\n Called: " + trace()[trace_len - 1].function
+        err_msg += "   on line: " + str(trace()[trace_len - 1].lineno)
+        err_msg += "\n code Context: " + \
+            str(trace()[trace_len - 1].code_context)
         err_msg += "\n ...." + p_message + "  ... " + str(sys.exc_info()[0])
         err_msg += "\n LOCALS ...." + str(sys.exc_info()[2].tb_frame.f_locals)
 #         pprint(str(sys.exc_info()[2].tb_frame.f_locals))
 #         print(json.dumps(sys.exc_info()[2].tb_frame.f_locals, indent=4))
-        print(err_msg)
-        print(); print(); print()
-               
-               
+
+        # Log it
+        log_message = f"{class_name}: : reportException: : exception created\n{err_msg}"
+        logger.log_info(log_message, to_print)
+
+        # Rollbar it
+        rollbar_on = True
+        if rollbar_on:
+            rollbar.init('8f67acbc427a4d6ba80c31516bd355da',
+                         'Mike Laptop')  # access_token, environment
+            rollbar.report_message(log_message)
+
+        if to_raise:
+            raise self
