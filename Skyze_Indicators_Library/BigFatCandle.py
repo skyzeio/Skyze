1e60dfa7e0f515760c9eff10b54db521bab3fffa
mikenew <mikenew16@gmail.com>
2017-11-01 16:15:06 +1100

    added ratio column to output

--- Skyze_Indicators_Library/BigFatCandle.py
+++ Skyze_Indicators_Library/BigFatCandle.py
@@ -1,8 +1,5 @@
-'''
-Created on 02/09/2017
-
-@author: michaelnew
-'''
+"""Created on 02/09/2017
+    @author: michaelnew"""
 # 3rd Party
 import pandas as pd
 
@@ -10,48 +7,53 @@ import pandas as pd
 from Skyze_Indicators_Library.IndicatorAbstract import IndicatorAbstract
 
 
-
 class BigFatCandle(IndicatorAbstract):
-    '''
-    classdocs
-    '''
+    """classdocs"""
 
     # Static Variables
-    name = "Big Fat Candle"
-    version = 1.0
+    _name = "Big Fat Candle"
+    _version = 1.0
 
     def __init__(self,
-                  p_fatRatio            # how big the body (o-c) of the candle should be .. range 0 to 1
-               ):
-        ''' Constructor '''
-        if p_fatRatio < 0 or p_fatRatio > 1 :
-#             raise exceptionality
+                 # how big the body (o-c) of the candle should be .. range 0 to 1
+                 p_fat_ratio
+                 ):
+        """Constructor"""
+        if p_fat_ratio < 0 or p_fat_ratio > 1:
+            #             raise exceptionality
             pass
 
-        self.fatRatio = p_fatRatio      # between 0 and 1
-        self.result = pd.DataFrame()
-        self.error = []
+        self._fat_ratio = p_fat_ratio      # between 0 and 1
+        self._result = pd.DataFrame()
+        self._error = []
 
     def inititial(self):
-        ''' Calculate the first value if the calc is different to the subsequent calculations '''
+        """Calculate the first value if the calc is different to the subsequent calculations"""
         pass
 
-    def calculate (self,
-                   p_data        # pd dataframe series
-                    ):
-        '''  Calculations '''
-        p_data["BigFatCandle"] = abs(p_data['Open']- p_data['Close'])/(p_data['High']-p_data['Low']) > self.fatRatio
+    def calculate(self,
+                  p_data        # pd dataframe series
+                  ):
+        """Calculations"""
+        p_data["BigFatCandle"] = abs(p_data['Open'] - p_data['Close']) \
+            / (p_data['High'] - p_data['Low']) \
+            >= self._fat_ratio
+        p_data["BFC_ratio"] = abs(p_data['Open'] - p_data['Close']) \
+            / (p_data['High'] - p_data['Low'])
 
-#         for data in p_market_data:
-#             abs(p_bar.open-p_bar.close)/(p_bar.high-p_bar.low) >= p_fatRatio
         return p_data
 
+    @classmethod
+    def getResult(self):
+        """ Getter """
+        return self._result
 
-
-    def getResult (self):
-        ''' Getter '''
-        return self.result
-
+    @classmethod
     def getName(self):
-        ''' Getter '''
-        return self.name
+        """ Getter """
+        return self._name
+
+    @classmethod
+    def getVersion(self):
+        """ Getter """
+        return self._version
