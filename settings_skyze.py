2466655000db3fbcbbf6d782af49ac609eacd6e0
mikenew <mikenew16@gmail.com>
2018-01-07 19:03:14 +1100

    Added exchanges

--- settings_skyze.py
+++ settings_skyze.py
@@ -16,7 +16,7 @@ test_results_file_path = "Unit_Test/Test_Results/"
 # {'1m': '1m', '5m': '5m', '15m': '15m', '30m': '30m', '1h': '1h',
 #   '3h': '3h', '6h': '6h', '12h': '12h', '1d': '1D', '1w': '7D',
 #   '2w': '14D', '1M': '1M'}
-dict_list = [
+data_intervals = [
     {"Name": "TICK",    "ccxt":   "",  "Seconds":     0, "Directory_name": "tick"},
     {"Name": "MIN_1",   "ccxt": "1m",  "Seconds":    60,
         "Directory_name": "minutes_1"},
@@ -35,32 +35,144 @@ dict_list = [
     {"Name": "MONTH_1", "ccxt": "1M",  "Seconds": 2592000, "Directory_name": "month_1"}
 ]
 
-intervals = pd.DataFrame(dict_list)
+intervals = pd.DataFrame(data_intervals)
 intervals = intervals.set_index('Name')
 
 # access by:   intervals.get_value("5_MIN","Seconds")
 
-
 # EXCHANGES
-exch_list = [
-    {"Name": "Coin Market Cap",
-        "Directory_name": "CMC",
-        "Class_name": "CoinMarketCap"},
-    {"Name": "Cryptopia",
-        "Directory_name": "Cryptopia",
-        "Class_name": "Cryptopia"},
-    {"Name": "Poloniex",
-        "Directory_name": "Poloniex",
-        "Class_name": "PoloniexSkyze"},
-    {"Name": "IGMarkets",
-        "Directory_name": "IGMarkets",
-        "Class_name": "IGMarkets"}
-]
+exchange_list = {
+    "Binance":
+        {"Name": "Binance",
+         "Directory_name": "Binance",
+         "Class_name": "binance",
+         "Data_intervals": ["DAY_1", "HOUR_4", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": "1d: 178 rows"},
 
-exchanges = pd.DataFrame(exch_list)
-exchanges = exchanges.set_index('Name')
+    "Bitfinex":
+        {"Name": "Bitfinex",
+         "Directory_name": "Bitfinex",
+         "Class_name": "bitfinex2",
+         "Data_intervals": ["DAY_1", "HOUR_3", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": "all: 120 rows"},
 
-# access by:   exchanges.get_value("Cryptopia","Directory_name")
+    "Bitmex":
+        {"Name": "Bitmex",
+         "Directory_name": "Bitmex",
+         "Class_name": "bitmex",
+         "Data_intervals": ["DAY_1", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": "1d: 100 rows"},
+
+    # no timeframes
+    "BitStamp":
+        {"Name": "BitStamp",
+         "Directory_name": "BitStamp",
+         "Class_name": "bitStamp",
+         "Data_intervals": [],
+         "ccxt_support": True,
+         "Data_provided": ""},
+
+    "Bittrex":
+        {"Name": "Bittrex",
+         "Directory_name": "Bittrex",
+         "Class_name": "bittrex",
+         "Data_intervals": ["DAY_1", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": "1d: 214 rows"},
+
+    "Bithumb":
+        {"Name": "Bithumb",
+         "Directory_name": "Bithumb",
+         "Class_name": "bithumb",
+         "Data_intervals": [],
+         "ccxt_support": True,
+         "Data_provided": ""},
+
+    "CCTX":
+        {"Name": "CCTX",
+         "Directory_name": "CCTX",
+         "Class_name": "cctx",
+         "Data_intervals": [],
+         "ccxt_support": True,
+         "Data_provided": ""},
+
+    # no timeframes
+    "CoinMarketCap":
+        {"Name": "CoinMarketCap",
+         "Directory_name": "CMC",
+         "Class_name": "CoinMarketCap",
+         "Data_intervals": ["DAY_1"],
+         "ccxt_support": False,
+         "Data_provided": ""},
+
+    "Cryptopia":
+        {"Name": "Cryptopia",
+         "Directory_name": "Cryptopia",
+         "Class_name": "Cryptopia",
+         "Data_intervals": ["TICK"],
+         "ccxt_support": False,
+         "Data_provided": ""},
+
+    "GDAX":
+        {"Name": "GDAX",
+         "Directory_name": "GDAX",
+         "Class_name": "gdax",
+         "Data_intervals": ["DAY_1", "HOUR_4", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": ""},
+
+    "HitBTC":
+        {"Name": "HitBTC",
+         "Directory_name": "HitBTC",
+         "Class_name": "hitbtc2",
+         "Data_intervals": ["DAY_1", "HOUR_4", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": "1d: 100"},
+
+    "Huobipro":
+        {"Name": "Huobipro",
+         "Directory_name": "Huobipro",
+         "Class_name": "huobipro",
+         "Data_intervals": ["DAY_1", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": "1d: 34 rows"},
+
+    "Kraken":
+        {"Name": "Kraken",
+         "Directory_name": "Kraken",
+         "Class_name": "kraken",
+         "Data_intervals": ["DAY_1", "HOUR_4", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": "1d: 160 rows"},
+
+    "OKEx":
+        {"Name": "OKEx",
+         "Directory_name": "OKEx",
+         "Class_name": "okex",
+         "Data_intervals": ["DAY_1", "HOUR_4", "HOUR_1", "MIN_5", "MIN_1"],
+         "ccxt_support": True,
+         "Data_provided": "1d: 17 rows"},
+
+    "Poloniex":
+        {"Name": "Poloniex",
+         "Directory_name": "Poloniex",
+         "Class_name": "PoloniexSkyze",
+         "Data_intervals": ["DAY_1", "HOUR_4", "HOUR_2", "MIN_5"],
+         "ccxt_support": False,
+         "Data_provided": "Full - 1d: 1329"},
+
+    "IGMarkets":
+        {"Name": "IGMarkets",
+         "Directory_name": "IGMarkets",
+         "Class_name": "IGMarkets",
+         "Data_intervals": [],
+         "ccxt_support": False,
+         "Data_provided": ""}
+}
+
 
 # ROLLBAR ERROR LOGGING
 rollbar_access_token = '8f67acbc427a4d6ba80c31516bd355da'
