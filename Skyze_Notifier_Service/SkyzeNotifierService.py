180af071ed6e59e54a684ce70ce430c14c5f15f4
mikenew <mikenew16@gmail.com>
2018-01-10 10:09:03 +1100

    Tweeting Notification added

--- Skyze_Notifier_Service/SkyzeNotifierService.py
+++ Skyze_Notifier_Service/SkyzeNotifierService.py
@@ -4,10 +4,20 @@
 # Third Party Imports
 from datetime import datetime
 import json
+import os
+
+# eMail
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# Twitter
+import tweepy
 
 # Skyze Imports
 from Skyze_Notifier_Service import settings
 from Skyze_Standard_Library.SkyzeServiceAbstract import *
+import Skyze_Standard_Library.Colourful_Printing as cp
 # Skyze Messages
 from Skyze_Messaging_Service.Messages.SkyzeMessageTypes import *
 from Skyze_Messaging_Service.Messages.MessageMarketDataUpdaterRunComplete \
@@ -15,40 +25,85 @@ from Skyze_Messaging_Service.Messages.MessageMarketDataUpdaterRunComplete \
 
 
 class SkyzeNotifierService(SkyzeServiceAbstract):
-    """Skyze inter-service message logger"""
+  """Skyze inter-service message logger"""
 
-    def __init__(self, message_bus):
-        """Constructor"""
-        path_to_service = "Skyze_Notifier_Service"
-        super().__init__(message_bus=message_bus, log_path=path_to_service)
+  def __init__(self, message_bus):
+    """Constructor"""
+    path_to_service = "Skyze_Notifier_Service"
+    super().__init__(message_bus=message_bus, log_path=path_to_service)
 
-    def __sendEmail(self, msg_subject, msg_content):
-        log_msg = f"{self.getType()}::sendEmail::NOT IMPLIMENTED::{msg_subject}\n{msg_content}"
-        self._logger.log_info(log_msg)
-        pass
+  def __sendEmail(self, msg_subject, msg_content):
+    ''' From https://coderwall.com/p/d4e_ya/using-python-to-sending-emails-via-aws'''
+    log_msg = f"{self.getType()}::sendEmail::NOT IMPLIMENTED::{msg_subject}\n{msg_content}"
+    self._logger.log_info(log_msg)
 
-    def __sendTweet(self, msg_subject, msg_content):
-        log_msg = f"{self.getType()}::sendTweet::NOT IMPLIMENTED::{msg_content}"
-        self._logger.log_info(log_msg)
-        pass
+    # AWS Config
+    # EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+    # EMAIL_HOST_USER = 'foo'
+    # EMAIL_HOST_PASSWORD = 'bar'
+    # EMAIL_PORT = 587
+    #
+    # msg = MIMEMultipart('alternative')
+    # msg['Subject'] = msg_subject
+    # msg['From'] = "Skyze Notifer"
+    # msg['To'] = "mikenew16@gmail.com"
+    #
+    # html = open('index.html').read()
+    #
+    # mime_text = MIMEText(html, 'html')
+    # msg.attach(mime_text)
+    #
+    # s = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+    # s.starttls()
+    # s.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+    # s.sendmail(me, you, msg_content)
+    # s.quit()
 
-    def __sendSMS(self, msg_subject, msg_content):
-        self.__sendTweet(msg_subject, msg_content)
+  def __get_twitter_api(self, cfg):
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+    return tweepy.API(auth)
 
-    def receiveMessage(self, message_received):
-        """Gets the mssage from the bus and routes internally"""
-        # Parent class processing
-        super().receiveMessage(message_received)
-        # Route to appropriate service
-        message_type = message_received.getMessageType()
-        if message_type == SkyzeMessageType.NOTIFICATION:
-            pass
-        elif message_type == SkyzeMessageType.MARKET_DATA_UPDATER_RUN_COMPLETE:
-            # TODO check user notification settings thensend apprpriately
-            msg_subject = "Mkt Data Update Complete: " \
-                + message_received.getMessageContent()
-            msg_content = msg_subject
-            self.__sendEmail(msg_subject, msg_content)
-            self.__sendSMS(msg_subject, msg_content)
-        else:
-            self._unknownMessageTypeError(message_received)
+  def __sendTweet(self, msg_subject, msg_content):
+    ''' uses tweepy to send a Tweet
+        Twitter apps https://apps.twitter.com/
+        http://nodotcom.org/python-twitter-tutorial.html'''
+    log_msg = f"{self.getType()}::sendTweet::UNDER DEVELOPMENT::{msg_content}"
+    self._logger.log_info(log_msg)
+    cp.prt(cp.pink("consumer_key"), os.environ['TWITTER_CONSUMER_KEY'],
+           "consumer_secret", os.environ['TWITTER_CONSUMER_SECRET'],
+           "access_token", os.environ['TWITTER_ACCESS_TOKEN'],
+           "access_token_secret", os.environ['TWITTER_TOKEN_SECRET'])
+    cfg = {
+        "consumer_key": os.environ['TWITTER_CONSUMER_KEY'],
+        "consumer_secret": os.environ['TWITTER_CONSUMER_SECRET'],
+        "access_token": os.environ['TWITTER_ACCESS_TOKEN'],
+        "access_token_secret": os.environ['TWITTER_TOKEN_SECRET']
+    }
+
+    api = self.__get_twitter_api(cfg)
+    tweet = f"{msg_subject} :: {msg_content}"
+    cp.prt(cp.pink(tweet))
+    status = api.update_status(status=tweet)
+    return
+
+  def __sendSMS(self, msg_subject, msg_content):
+    self.__sendTweet(msg_subject, msg_content)
+
+  def receiveMessage(self, message_received):
+    """Gets the mssage from the bus and routes internally"""
+    # Parent class processing
+    super().receiveMessage(message_received)
+    # Route to appropriate service
+    message_type = message_received.getMessageType()
+    if message_type == SkyzeMessageType.NOTIFICATION:
+      pass
+    elif message_type == SkyzeMessageType.MARKET_DATA_UPDATER_RUN_COMPLETE:
+      # TODO check user notification settings thensend apprpriately
+      msg_subject = "Mkt Data Update Complete: " \
+          + message_received.getMessageContent()
+      msg_content = msg_subject
+      self.__sendEmail(msg_subject, msg_content)
+      self.__sendSMS(msg_subject, msg_content)
+    else:
+      self._unknownMessageTypeError(message_received)
---
+++ Skyze_Notifier_Service/Unit_Test/SkyzeNotifierService_test.py
@@ -0,0 +1,60 @@
+"""Created on 9 / 1 / 2018 @author: michaelnew"""
+
+
+# Standard Libraries
+import ccxt
+
+# Skyze libraries - Required
+from Skyze_Standard_Library.Unit_Test.UnitTestSkyzeAbstract import *
+import Skyze_Standard_Library.Colourful_Printing as cp
+
+# Skyze libraries - Test Case Specific
+from Skyze_Notifier_Service.SkyzeNotifierService import *
+from Skyze_Messaging_Service.SkyzeMessageBusService import *
+from Skyze_Messaging_Service.Messages.MessageMarketDataUpdaterRunComplete \
+    import MessageMarketDataUpdaterRunComplete
+
+
+class SkyzeNotifierService_test(UnitTestSkyzeAbstract):
+  """Test the SkyzeNotifierService Class"""
+
+  _package_name = "Skyze_Notifer_Service"
+  _class_tested = "SkyzeNotifierService"
+
+  #@unittest.skip("Working on new test")
+  def test__sendTweet(self):
+    # Set up for testing
+    # logger_class_name = self.__class__.__name__
+    # logger = SkyzeLogger(logger_class_name, "")
+    # log_message = f"{logger_class_name}::__init__::Started"
+    package_name = "Skyze Notifier Service"
+    test_name = "__sendTweet"
+    test_file = target_file = test_columns = "N/A"
+    self.printTestHeader(package_name, test_name, test_file,
+                         target_file, test_columns)
+
+    message_bus = SkyzeMessageBusService()
+    notifier_service = SkyzeNotifierService(message_bus)
+
+    source_name = "Bitfinex"
+    exchange_intervals = []
+    error_list = []
+
+    print("\n\tsource_name: " + source_name +
+          "\texchange_intervals: " + str(exchange_intervals) +
+          "\terror_list: " + str(error_list))
+
+    end_run_msg = MessageMarketDataUpdaterRunComplete(
+        source_name, exchange_intervals, len(error_list),
+        error_list, market_pairs=None)
+    notifier_service.receiveMessage(end_run_msg)
+
+    # Testing
+    # notifier_service = SkyzeNotifierService(message_bus)
+    # msg_subject = "Testing"
+    # msg_content = "Tweet-a-Test-a-Tweet-a-Test-a-Tweet-a-Test-a-Tweet-a-Test-a-"
+    # notifier_service.__sendTweet(self, msg_subject, msg_content)
+
+
+if __name__ == '__main__':
+  unittest.main()
